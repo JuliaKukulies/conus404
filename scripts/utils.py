corrected precip volume computation for mcs classification

scripts/utils.py
@@ -164,14 +164,16 @@ def process_clusters(tracks):
     gb_clusters = tracks.groupby("cluster")
     
     clusters = gb_clusters.track.first().to_frame().rename(columns=dict(track="cluster_track_id"))
+    
     clusters["cluster_time"] = gb_clusters.time.first().to_numpy()
-    clusters["cluster_longitude"] = gb_clusters.apply(lambda x:weighted_circmean(x.lon.to_numpy(), x.area.to_numpy(), low=0, high=360))
-    clusters["cluster_latitude"] = gb_clusters.apply(lambda x:np.average(x.lat.to_numpy(), weights=x.area.to_numpy()))
+    
+    clusters["cluster_longitude"] = gb_clusters.apply(lambda x:weighted_circmean(x.lon.to_numpy(), x.area.to_numpy(), low=0, high=360))#, include_groups=False)
+    clusters["cluster_latitude"] = gb_clusters.apply(lambda x:np.average(x.lat.to_numpy(), weights=x.area.to_numpy()))#, include_groups=False)
     
     clusters["cluster_area"] = gb_clusters.area.sum().to_numpy()
     clusters["cluster_max_precip"] = gb_clusters.max_precip.max().to_numpy()
     clusters["cluster_total_precip"] = gb_clusters.total_precip.sum().to_numpy()
-    
+    clusters["cluster_total_precip_volume"] = gb_clusters.total_precip.sum().to_numpy() * gb_clusters.area.sum().to_numpy()
     return tracks, clusters
 
 
@@ -188,11 +190,11 @@ def is_track_mcs_cluster(clusters: pd.DataFrame) -> pd.DataFrame:
     pd.DataFrame
         _description_
     """
-    consecutive_precip_max = clusters.groupby(["cluster_track_id"]).cluster_max_precip.apply(lambda x:max_consecutive_true(x>=10))
+    consecutive_precip_max = clusters.groupby(["cluster_track_id"]).cluster_max_precip.apply(lambda x:max_consecutive_true(x>=10))#, include_groups=False)
     
-    consecutive_area_max = clusters.groupby(["cluster_track_id"]).cluster_area.apply(lambda x:max_consecutive_true(x>=4e4))
+    consecutive_area_max = clusters.groupby(["cluster_track_id"]).cluster_area.apply(lambda x:max_consecutive_true(x>=4e4))#, include_groups=False)
     
-    max_total_precip_volume = clusters.groupby(["cluster_track_id"]).cluster_total_precip.max() * 16
+    max_total_precip = clusters.groupby(["cluster_track_id"]).cluster_total_precip_volume.max() 
     
     is_mcs = np.logical_and.reduce(
         [
@@ -204,6 +206,8 @@ def is_track_mcs_cluster(clusters: pd.DataFrame) -> pd.DataFrame:
     mcs_tracks =  pd.Series(data=is_mcs, index=consecutive_precip_max.index)
     mcs_tracks.index.name="track"
     return mcs_tracks
+
+
 
 
 def regrid_data(era_var, conus): 