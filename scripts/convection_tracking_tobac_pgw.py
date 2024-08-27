383a36e7752928b6adcd9dac42b013c2011270f2
JuliaKukulies <kukulies@ucar.edu>
2024-08-27 16:59:37 -0600

    added C404 postprocessing and tracking scripts for PGW run

---
+++ scripts/convection_tracking_tobac_pgw.py
@@ -0,0 +1,154 @@
+
+
+
+"""
+This script is used to track convective storms based on IR brightness temperatures and surface precipitation in the CONUS404 dataset using the tracking library tobac. Mesoscale convective systems are a subset of the resulting storm database. These are identified using extra criteria for a large cloud shield <= 241 K and the occurrence of a cold core and heavy surface precipitation. 
+
+The script also calculates bulk statistics (incl. total precipitation, condensation and ice water path) for each detected storm feature. These statistics can be used to estimate storm-scale precipitation efficiencies. 
+
+Contact: kukulies@ucar.edu
+
+"""
+
+import sys
+import numpy as np 
+from pathlib import Path 
+import xarray as xr 
+import pandas as pd 
+import warnings
+warnings.filterwarnings("ignore")
+import utils 
+import tobac
+print('using tobac version ', tobac.__version__, flush = True) 
+
+#### input parameter ####
+year = str(sys.argv[1])
+
+#### DIRECTORIES ####
+
+# processed CONUS404 data with monthly files with hourly 2D variables needed for the calculation 
+data2d = Path('/glade/campaign/mmm/c3we/CPTP_kukulies/conus404/PGW/')
+monthly_files = list(data2d.glob( str('conus404_' + year + '*.nc')))  
+print(len(monthly_files), ' files detected for year ', year, flush = True)
+monthly_files.sort()
+
+# data path
+savedir = Path('/glade/campaign/mmm/c3we/CPTP_kukulies/conus404/tracked_storms/')
+
+### TRACKING PARAMETERS ###
+# 4km horizontal grid spacing, hourly data (in meter and seconds)
+dxy,dt= 4000, 3600 
+
+parameters_features={}
+parameters_features['position_threshold']='weighted_diff'
+parameters_features['sigma_threshold']= 0.5
+parameters_features['n_min_threshold']= 10 
+parameters_features['target']='minimum'
+parameters_features['threshold']=[241, 230, 225, 220, 210, 200]
+parameters_features['statistic'] = {"feature_min_tb": np.nanmin, 'feature_max_iwp': np.nanmax, 'feature_mean_tb': np.nanmean}
+parameters_features['position_threshold'] = "center"
+
+parameters_linking={}
+parameters_linking['d_max']=10*dxy
+parameters_linking['stubs']= 2  
+parameters_linking['adaptive_stop']=0.2
+parameters_linking['adaptive_step']=0.95
+parameters_linking['method_linking']= 'predict'
+
+parameters_segmentation = {}
+parameters_segmentation['threshold']=242
+parameters_segmentation['target'] = "minimum"
+parameters_segmentation['statistic'] = {"object_min_tb": np.nanmin, 'object_max_tb': np.nanmax, 'object_mean_tb': np.nanmean}
+
+parameters_merge = dict(
+    distance=dxy*20,)
+################################ processing monthly files ######################################
+
+for monthly_file in monthly_files:
+    month =  str(monthly_file)[-5:-3]
+    output_file = savedir / str('tobac_storm_tracks_' + year + '_' + month + '.nc')
+    print(monthly_file, month, flush = True)
+    if output_file.is_file() is False:
+        ds = xr.open_dataset(monthly_file)
+        # make longitudes and latitudes coordinates (instead of variables) 
+        coords = {'lon': (["south_north", "west_east"], ds.lons.values), 'lat': (["south_north", "west_east"], ds.lats.values)}
+        ds = ds.assign_coords(coords)
+        ds = ds.drop_vars(["lats", "lons"])
+
+        # convert tracking fields to iris cubes 
+        tb_iris = ds.tb.to_iris()
+        precip_iris = ds.surface_precip.to_iris()
+
+        # feature detection based on Tb
+        print(f"Commencing feature detection using Tb for ", year, month, flush=True)
+        features=tobac.feature_detection_multithreshold(tb_iris ,dxy, **parameters_features)
+
+        # linking  
+        print(f"Commencing tracking", flush=True)
+        tracks = tobac.linking_trackpy(features, tb_iris, dt, dxy, **parameters_linking)
+        # reduce tracks to valid cells and those cells that contain a cold core
+        tracks = tracks[tracks.cell != -1]
+        tracks_cold_core = tracks.groupby("cell").feature_min_tb.min()
+        valid_cells = tracks_cold_core.index[tracks_cold_core < 225]
+        tracks = tracks[np.isin(tracks.cell, valid_cells)]
+
+        # use merging and splitting module and perform segmentation
+        print(f"Calculating merges and splits", flush = True )
+        merges = tobac.merge_split.merge_split_MEST(tracks, dxy, **parameters_merge)
+        # add track identifiers to feature dataframe 
+        tracks["track"] = merges.feature_parent_track_id.data.astype(np.int64)
+        track_start_time = tracks.groupby("track").time.min()
+        tracks["time_track"] = tracks.time - track_start_time[tracks.track].to_numpy()
+
+        # Tb segmentation 
+        print(f"Commencing segmentation", flush=True)
+        mask, tracks = tobac.segmentation_2D(tracks, tb_iris, dxy, **parameters_segmentation)
+
+        # Bulk statistics for identified cloud objects
+        print('Calculating the statistics...', flush = True)
+        tracks = utils.get_statistics_conus(tracks, mask, ds, inplace = True)
+
+        # MCS classification
+        tracks, clusters = utils.process_clusters(tracks)
+        mcs_flag = utils.is_track_mcs_cluster(clusters)
+
+        # A little check for the MCS flag result 
+        print(mcs_flag[mcs_flag == True].shape[0], 'identified storms are MCSs', flush = True)
+        assert np.unique(tracks.track.values).size == mcs_flag.shape[0]
+
+        # OUTPUT DATA FRAME
+        # remove redundant columns 
+        redundant = ['idx', 'num', 'timestr', 'time_cell']
+        tracks.drop(redundant, axis = 1, inplace= True)
+        # checks 
+        assert merges.track_child_cell_count.shape == mcs_flag.shape
+        assert merges.cell_parent_track_id.shape == merges.cell_child_feature_count.shape
+
+        # Add MCS flag (per track) 
+        df = mcs_flag.rename('mcs_flag').to_frame()
+        tracks = tracks.merge(df, on='track', how='left') 
+        # Add how many cells belong to each track (per track) 
+        tracks = tracks.merge(merges.track_child_cell_count.to_dataframe(), on='track', how='left')   
+        # Add ID of parent track (per cell) 
+        tracks = tracks.merge( merges.cell_parent_track_id.to_dataframe(), on='cell', how='left') 
+        # Add how many features belong to each cell (per cell) 
+        tracks = tracks.merge( merges.cell_child_feature_count.to_dataframe(), on='cell', how='left')
+        # Add whether the cell starts with a split (per cell) 
+        #tracks =tracks.merge( merges.cell_starts_with_split.to_dataframe(), on='cell', how='left')
+        # Addinfo whether cell ends with a merge (per cell)
+        #tracks =tracks.merge( merges.cell_ends_with_merge.to_dataframe(), on='cell', how='left')
+
+        # Save output data (mask and track files)
+        xr.DataArray.from_iris(mask).to_netcdf(savedir / str('tobac_storm_mask_' + year + '_' + month + '.nc'))
+        tracks.to_xarray().to_netcdf(savedir / str('tobac_storm_tracks_' + year + '_' + month + '.nc'))    
+        print('files saved', flush = True)
+
+
+    else:
+        print(str(monthly_file), ' does already exist.', flush = True)
+        continue
+
+    
+
+
+    
---
+++ scripts/postprocessing_variables_conus404_pgw.py
@@ -0,0 +1,188 @@
+"""
+This script produces monthly files of IWP, LWP, condensation rates, surface precipitation and brightness temperatures from standard 3D model WRF output.
+
+kukulies@ucar.edu
+
+"""
+
+from pathlib import Path 
+import xarray as xr 
+import numpy as np 
+import wrf 
+from microphysics import microphysics_functions as micro 
+from netCDF4 import Dataset
+import datetime 
+import sys
+
+########################### Directory with model output #####################################
+
+path = Path('/glade/campaign/ncar/USGS_Water/CONUS404_PGW/') 
+out = Path('/glade/campaign/mmm/c3we/CPTP_kukulies/conus404/PGW/')
+
+year_in = int(sys.argv[1])
+mon_in = int(sys.argv[2])
+years = np.arange(year_in, year_in  + 1 )
+
+################################## Function for OLR-Tb conversion ###########################
+
+def get_tb(olr):
+    """                                                                                                       
+    This function converts outgoing longwave radiation to brightness temperatures.                             
+    using the Planck constant.                                                                                 
+                                                                                                              
+    Args:                                                                                                       
+        olr(xr.DataArray or numpy array): 2D field of model output with OLR                                      
+    Returns:                                                                                                    
+        tb(xr.DataArray or numpy array): 2D field with estimated brightness temperatures                        
+    """
+    # constants                                                                                                
+    aa = 1.228
+    bb = -1.106 * 10**(-3) # K−1                                                                              
+    # Planck constant                                                                                          
+    sig = 5.670374419 * 10**(-8) # W⋅m−2⋅K−4                                                                    
+    # flux equivalent brightness temperature                                                                   
+    Tf = (abs(olr)/sig) ** (1./4)
+    tb = (((aa ** 2 + 4 * bb *Tf ) ** (1./2)) - aa)/(2*bb)
+    return tb 
+
+def wstagger_to_mass(W):
+    """                                                                                                         
+    W are the data on the top and bottom of a grid box                                                          
+    A simple conversion of the stagger grid to the mass points.                                                 
+                                                                                                                
+    (row_j1+row_j2)/2 = masspoint_inrow                                                                         
+                                                                                                                
+    Input:                                                                                                      
+        Wgrid with size (##+1)                                                                                  
+    Output:                                                                                                     
+        W on mass points with size (##)                                                                         
+    """
+    # create the first column manually to initialize the array with correct dimensions                          
+    W_masspoint = (W[0, :, :]+W[1, :, :])/2. # average of first and second column    
+    W_masspoint = np.expand_dims(W_masspoint, 0)
+
+    W_num_levels = int(W.shape[0])-1 # we want one less level than we have                                      
+
+    # Loop through the rest of the rows                                                                         
+    # We want the same number of rows as we have columns.                                                       
+    # Take the first and second row, average them, and store in first row in V_masspoint                        
+    for lev in range(1,W_num_levels):
+        lev_avg = (W[lev, :, :]+W[lev+1, :, :])/2.
+        lev_avg = np.expand_dims(lev_avg, 0)
+        W_masspoint = np.vstack((W_masspoint,lev_avg))
+    return W_masspoint
+
+##############################################################################################
+year = year_in
+month = mon_in
+print('input received:', str(year), str(month), flush = True)
+
+
+# check first if month has already been processed 
+out_file = out / ( 'conus404_' + str(year) + str(month).zfill(2) +  '.nc')
+if out_file.is_file():
+    print(out_file, ' already processed.',  flush = True)
+else:
+    # subdirectory for specific month
+    if int(month) >= 10:
+        wyyear = year - 1
+        print('water year ', str(wyyear), flush = True )
+    else:
+        wyyear = year 
+    monthly_path = path / ('wy' + str(year)) / (str(wyyear) + str(month).zfill(2))
+    hourly_files_3d = list(monthly_path.glob('*wrf3d*')) 
+    hourly_files_2d = list(monthly_path.glob('*wrf2d*'))
+    hourly_files_3d.sort()
+    hourly_files_2d.sort()
+    assert len(hourly_files_3d) == len(hourly_files_3d)
+
+    # open file and extract necessary variables 
+    print('processing variables for', str(year) +' '+ str(month), ' over ',len(hourly_files_2d), 'files', flush = True)
+    print(datetime.datetime.now(), flush = True)
+    for file_idx, fname in enumerate(hourly_files_2d):
+        ds2d = xr.open_dataset(fname).squeeze()
+        ds3d = xr.open_dataset(hourly_files_3d[file_idx]).squeeze()
+        wrfin = Dataset(hourly_files_3d[file_idx])
+
+        # 3d variables 
+        iwc = ds3d.QSNOW + ds3d.QGRAUP + ds3d.QICE
+        lwc = ds3d.QRAIN + ds3d.QCLOUD 
+
+        # get variables that are necessary for calculation of condensation rates
+        w_staggered = ds3d.W
+        vertical_velocity = wstagger_to_mass(w_staggered)
+        temp = ds3d.TK
+        pressure = ds3d.P
+        qcloud = ds3d.QCLOUD
+
+        # 2d variables 
+        precip = ds2d.PREC_ACC_NC.data 
+        olr = ds2d.OLR.data
+
+        ############ calculate quantities from standard output: LWP, IWP, C, P, Tb ###################    
+        brightness_temperature = get_tb(olr)
+        # integate iwc and lwp over pressure                                                                    
+        iwp = micro.pressure_integration(iwc.data, -pressure.data)
+        lwp = micro.pressure_integration(lwc.data, -pressure.data)
+        # condensation rate from w,P,T,qcloud
+        condensation_rate_t = micro.get_condensation_rate(vertical_velocity, temp, pressure)
+        condensation_cloud = condensation_rate_t.where(qcloud > 0, 0 )
+        condensation_masked = condensation_cloud.where(condensation_cloud > 0, 0 ).data
+        condensation_integrated = micro.pressure_integration(condensation_masked,-pressure.data)
+        # concatenate processed variables along time axis 
+        if file_idx == 0:
+            tiwp = iwp
+            tlwp = lwp
+            condensation_rate = condensation_integrated
+            surface_precip = precip
+            tb = brightness_temperature
+            time = np.array(ds2d.Time.values) 
+        else:
+            surface_precip = np.dstack((surface_precip, precip))
+            tiwp  = np.dstack((tiwp, iwp ))
+            tlwp = np.dstack((tlwp, lwp))
+            condensation_rate = np.dstack((condensation_rate, condensation_integrated))
+            tb = np.dstack((tb, brightness_temperature))
+            time = np.append(time, ds2d.Time.values)
+
+
+        # get coords
+        lats = ds2d.XLAT.values
+        lons = ds2d.XLONG.values
+        south_north = ds2d.south_north.values
+        west_east = ds2d.west_east.values 
+
+        # close datasets and delete variables
+        ds3d.close()
+        ds2d.close()
+        wrfin.close()
+
+        del iwc, iwp, lwc, lwp
+        del temp, pressure, qcloud, vertical_velocity
+        del precip, olr
+        del condensation_cloud, condensation_integrated, condensation_masked
+
+    # write new netCDF file for month 
+    data_vars = dict(tiwp=(["south_north", "west_east", "time"], tiwp),
+                     tlwp=(["south_north", "west_east", "time"], tlwp),
+                     surface_precip=(["south_north", "west_east", "time"],surface_precip),
+                     condensation_rate=(["south_north", "west_east", "time"], condensation_rate),
+                     tb=(["south_north", "west_east", "time"], tb),
+                     lats=(["south_north", "west_east"], lats),
+                     lons=(["south_north", "west_east"], lons),)
+
+    print(datetime.datetime.now(), flush = True)
+    coords = dict(south_north=south_north, west_east=west_east, time = time)
+    data = xr.Dataset(data_vars=data_vars, coords=coords)
+    data.to_netcdf( out / ( 'conus404_' + str(wyyear) + str(month).zfill(2) +  '.nc') )  
+
+    # close datasets and delete variables     
+    del tiwp, tlwp
+    del surface_precip, condensation_rate
+    del lats, lons, tb, time 
+    data.close()
+
+
+
+
+
